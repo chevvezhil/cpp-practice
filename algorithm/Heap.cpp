268d073e3628689a55d9ae79400eceb0fecb00c6
Chevvanthi <chevvanthi.e@inxsasia.com>
2020-07-09 13:20:38 +0530

    KMP algo

---
+++ algorithm/Heap.cpp
@@ -0,0 +1,25 @@
+// KMP used to find the string pattern matching 
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+bool KMP(string text, string pattern){
+ 
+  int prefix[];
+
+
+}
+
+int main(){
+
+ string text , pattern ;
+ cin >> text ; cin >> pattern;
+
+ if(KMP(text, pattern))
+    cout << "pattern matches with the given string";
+else 
+    cout << "pattern doesn't matches";
+
+}
---
+++ algorithm/KMPAlgo.cpp
@@ -0,0 +1,69 @@
+// KMP used to find the string pattern matching 
+#include<iostream>
+#include<string>
+#include<array>
+
+using namespace std;
+
+void computePrefix(int prefix[],string pattern){
+
+  int index =0;
+
+  for(int i=1; i< pattern.length();){
+
+    if(pattern[index] == pattern[i]){
+      prefix[i] = index+1;
+      index++;
+      i++;
+    }else{
+       if(index !=0){
+        index = prefix[index-1];
+       }else{
+         prefix[i] =0;
+         i++;
+       }
+
+    }
+
+  }
+
+}
+
+bool KMP(string text, string pattern){
+ 
+  int prefix[pattern.length()];
+
+  computePrefix(prefix,pattern);
+
+  int i=0,j=0;
+  while (i <text.length() && j < pattern.length()){
+    
+    if(text[i] == pattern[j]){
+        i++;
+        j++;
+    }else{
+      if( j !=0){
+        j = prefix[j-1];
+      }else{
+        i++;
+      }
+    }
+  }
+  
+  if(j == pattern.length())
+    return true;
+  
+  return false;
+}
+
+int main(){
+
+ string text , pattern ;
+ cin >> text ; cin >> pattern;
+
+ if(KMP(text, pattern))
+    cout << "pattern matches with the given string";
+else 
+    cout << "pattern doesn't matches";
+
+}
